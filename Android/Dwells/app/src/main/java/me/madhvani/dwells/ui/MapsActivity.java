58473f98af918dd0f4f6164c885211996c2e7dd0
Anthony Madhvani <anny.mdvn@gmail.com>
2015-04-13 11:50:11 +0200

    Set project structure

--- Android/Dwells/app/src/main/java/me/madhvani/dwells/ui/MapsActivity.java
+++ Android/Dwells/app/src/main/java/me/madhvani/dwells/ui/MapsActivity.java
@@ -1,4 +1,4 @@
-package me.madhvani.dwells;
+package me.madhvani.dwells.ui;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -7,6 +7,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import me.madhvani.dwells.R;
 
 public class MapsActivity extends FragmentActivity {
 
