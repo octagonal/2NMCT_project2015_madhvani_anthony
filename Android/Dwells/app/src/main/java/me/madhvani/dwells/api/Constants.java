f12c7e2fcf848c357b9bd6f6b5a220effe270d50
Anthony Madhvani <anny.mdvn@gmail.com>
2015-04-30 18:11:22 +0200

    Add iKot Endpoint

--- Android/Dwells/app/src/main/java/me/madhvani/dwells/api/Constants.java
+++ Android/Dwells/app/src/main/java/me/madhvani/dwells/api/Constants.java
@@ -5,4 +5,5 @@ package me.madhvani.dwells.api;
  */
 public class Constants {
     public final static String ENDPOINT = "http://madhvani.me/api";
+    public final static String KOT_PROVIDER_ENDPOINT = "http://ikot.be";
 }
