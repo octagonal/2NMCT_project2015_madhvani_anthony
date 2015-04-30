6fff5f01e9d578c26e30f282f81327b1b81fadc1
Anthony Madhvani <anny.mdvn@gmail.com>
2015-04-30 18:11:53 +0200

    Set up KotDetail WebView

--- Android/Dwells/app/src/main/java/me/madhvani/dwells/ui/KotDetail.java
+++ Android/Dwells/app/src/main/java/me/madhvani/dwells/ui/KotDetail.java
@@ -12,8 +12,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import me.madhvani.dwells.R;
+import me.madhvani.dwells.api.Constants;
 import me.madhvani.dwells.model.Kot;
 
 public class KotDetail extends Activity {
@@ -23,21 +30,18 @@ public class KotDetail extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.inject(this);
         setContentView(R.layout.activity_kot_detail);
         if (savedInstanceState == null) {
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
             WebViewFragment fragment = new WebViewFragment();
+
+            fragment.setArguments(getIntent().getExtras());
             fragmentTransaction.add(R.id.container, fragment);
             fragmentTransaction.commit();
-
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new WebViewFragment())
-                    .commit();
         }
-        Intent intent = getIntent();
-        Kot kot = intent.getExtras().getParcelable("kot");
-        Log.i(KOT_DETAIL, kot.getUrl() );
     }
 
 
@@ -68,6 +72,9 @@ public class KotDetail extends Activity {
      */
     public static class WebViewFragment extends Fragment {
 
+        @InjectView(R.id.webView)
+        WebView kotView;
+
         public WebViewFragment() {
         }
 
@@ -75,6 +82,25 @@ public class KotDetail extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_kot_detail, container, false);
+
+            ButterKnife.inject(this, rootView);
+
+            Kot kot = getArguments().getParcelable("kot");
+
+            kotView.loadUrl(Constants.KOT_PROVIDER_ENDPOINT + kot.getUrl());
+
+            kotView.setWebViewClient(new WebViewClient() {
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                    return super.shouldInterceptRequest(view, request);
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+
+                }
+
+            });
             return rootView;
         }
     }
