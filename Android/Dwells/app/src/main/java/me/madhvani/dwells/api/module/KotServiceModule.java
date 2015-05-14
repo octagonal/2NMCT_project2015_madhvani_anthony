7426eba5ad8bf4978641db4fdf78fb4fce46d8f6
Anthony Madhvani <anny.mdvn@gmail.com>
2015-05-14 18:47:26 +0200

    Provide KotServiceAPI

--- Android/Dwells/app/src/main/java/me/madhvani/dwells/api/module/KotServiceModule.java
+++ Android/Dwells/app/src/main/java/me/madhvani/dwells/api/module/KotServiceModule.java
@@ -1,7 +1,19 @@
 package me.madhvani.dwells.api.module;
 
+import dagger.Module;
+import dagger.Provides;
+import me.madhvani.dwells.api.KotServiceAPI;
+import retrofit.RestAdapter;
+
 /**
  * Created by anthony on 15.14.5.
  */
+
+@Module(includes = RestAdapterModule.class)
 public class KotServiceModule {
+    @Provides
+    KotServiceAPI provideKotService(RestAdapter restAdapter){
+        KotServiceAPI service = restAdapter.create(KotServiceAPI.class);
+        return service;
+    }
 }
