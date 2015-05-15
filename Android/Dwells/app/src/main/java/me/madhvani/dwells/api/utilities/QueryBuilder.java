6a2a79d35a0dd7d195494e9752a67ae087300420
Anthony Madhvani <anny.mdvn@gmail.com>
2015-05-15 10:05:03 +0200

    Make String helper method for queries

--- Android/Dwells/app/src/main/java/me/madhvani/dwells/api/utilities/QueryBuilder.java
+++ Android/Dwells/app/src/main/java/me/madhvani/dwells/api/utilities/QueryBuilder.java
@@ -4,4 +4,7 @@ package me.madhvani.dwells.api.utilities;
  * Created by anthony on 15.14.5.
  */
 public class QueryBuilder {
+    public static String StringBuilder(String input){
+        return "eq." + input;
+    }
 }
