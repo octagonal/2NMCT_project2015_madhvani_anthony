ec0b84e1158123419d19968e3baf8e9bf429b474
Anthony Madhvani <anny.mdvn@gmail.com>
2015-05-16 13:20:56 +0200

    Add Bookmarks Card UI

--- Android/Dwells/app/src/main/java/me/madhvani/dwells/ui/MapsActivity.java
+++ Android/Dwells/app/src/main/java/me/madhvani/dwells/ui/MapsActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +28,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -46,8 +49,6 @@ public class MapsActivity extends FragmentActivity {
 
     //TODO: Zet steden LatLng's in aparte struct
     private static final LatLng GENT = new LatLng(51.0878316,3.7237548);
-
-    MyPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +71,7 @@ public class MapsActivity extends FragmentActivity {
     }
 
     public static class MyPagerAdapter extends FragmentPagerAdapter implements PagerSlidingTabStrip.IconTabProvider {
-        private static int NUM_ITEMS = 2;
+        private static int NUM_ITEMS = 1;
 
         private int tabIcons[] = {R.drawable.ic_tab_map, R.drawable.ic_tab_bookmarks};
 
@@ -88,10 +89,10 @@ public class MapsActivity extends FragmentActivity {
         @Override
         public Fragment getItem(int position) {
             switch (position) {
-                case 0: // Fragment # 0 - This will show FirstFragment
-                    return MapFragment.newInstance(0, "Page # 1");
-                case 1: // Fragment # 0 - This will show FirstFragment different title
-                    return BookmarksFragment.newInstance(1, "Page # 2");
+                case 0:
+                    return MapFragment.newInstance(0);
+                //case 1:
+                  //  return BookmarksFragment.newInstance(1);
                 default:
                     return null;
             }
@@ -104,19 +105,45 @@ public class MapsActivity extends FragmentActivity {
 
     }
 
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
+        protected TextView title;
+
+        public ContactViewHolder(View v) {
+            super(v);
+            title =  (TextView) v.findViewById(R.id.title);
+        }
+    }
+
     public static class BookmarksFragment extends Fragment {
         public static final String ARG_PAGE = "ARG_PAGE";
         private int page;
+        private static RecyclerView recyclerView;
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.test_fragment, container, false);
-            TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
-            tvTitle.setText("Fragment #" + page);
+            View view = inflater.inflate(R.layout.bookmarks_fragment, container, false);
+            recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+            // Setup layout manager for items
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+            // Control orientation of the items
+            layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            layoutManager.scrollToPosition(0);
+            // Attach layout manager
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setHasFixedSize(true);
+            // Bind adapter to recycler
+            ArrayList<String> items = new ArrayList<String>();
+            items.add(0,"lol");
+            items.add(1,"lel");
+            items.add(2,"kek");
+
+            SimpleRecyclerAdapter aItems = new SimpleRecyclerAdapter(items);
+            recyclerView.setAdapter(aItems);
             return view;
         }
 
-        public static BookmarksFragment newInstance(int page, String title) {
+        public static BookmarksFragment newInstance(int page) {
             BookmarksFragment fragmentFirst = new BookmarksFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_PAGE, page);
@@ -182,7 +209,7 @@ public class MapsActivity extends FragmentActivity {
         public static MapView mapView;
 
         // newInstance constructor for creating fragment with arguments
-        public static MapFragment newInstance(int page, String title) {
+        public static MapFragment newInstance(int page) {
             MapFragment fragmentFirst = new MapFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_PAGE, page);
@@ -204,11 +231,6 @@ public class MapsActivity extends FragmentActivity {
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            if(mMap == null){
-                mMap = googleMap;
-                if (mMap != null) {
-                }
-            }
             setUpMap();
             Log.v(TAG,"MapView already initialized");
         }
@@ -295,6 +317,41 @@ public class MapsActivity extends FragmentActivity {
 
                 }
             });
+        }
+    }
+
+    public static class SimpleRecyclerAdapter extends RecyclerView.Adapter<ContactViewHolder> {
+        private List<String> items;
+
+        // Provide a reference to the views for each data item
+        // Provide access to all the views for a data item in a view holder
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public SimpleRecyclerAdapter(List<String> items) {
+            this.items = items;
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return this.items.size();
+        }
+
+        // Create new items (invoked by the layout manager)
+        // Usually involves inflating a layout from XML and returning the holder
+        @Override
+        public ContactViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            View itemView = LayoutInflater.from(viewGroup.getContext()).
+                    inflate(R.layout.bookmarks_card, viewGroup, false);
+            return new ContactViewHolder(itemView);
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        // Involves populating data into the item through holder
+        @Override
+        public void onBindViewHolder(ContactViewHolder viewHolder, int position) {
+            String item = items.get(position);
+            viewHolder.title.setText(item);
         }
     }
 
